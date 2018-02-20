9c2de51671b8cf080be38b7bcfd96ab480e3524f
RaviShankarArya <rshankarganeshrao@gmail.com>
2018-02-20 11:55:01 +0530

    added file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,9 +6,37 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
+#Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+#Rails.application.config.assets.precompile += %w( admin.js admin.css )
+ Rails.application.config.assets.precompile += %w( animate.css )
+ Rails.application.config.assets.precompile += %w( bootstrap.css )
+ Rails.application.config.assets.precompile += %w( flexslider.css )
+ Rails.application.config.assets.precompile += %w( general.css )
+ Rails.application.config.assets.precompile += %w( icomoon.css )
+ Rails.application.config.assets.precompile += %w( magnific-popup.css )
+ Rails.application.config.assets.precompile += %w( my_style.css )
+ Rails.application.config.assets.precompile += %w( owl.carousel.css )
+ Rails.application.config.assets.precompile += %w( owl.theme.default.min.css )
+ Rails.application.config.assets.precompile += %w( style.css )
+ Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+ Rails.application.config.assets.precompile += %w( rails-ujs.js )
+ Rails.application.config.assets.precompile += %w( cable.js )
+ Rails.application.config.assets.precompile += %w( google_map.js )
+ Rails.application.config.assets.precompile += %w( jquery.countTo.js )
+ Rails.application.config.assets.precompile += %w( jquery.easing.1.3.js )
+ Rails.application.config.assets.precompile += %w( jquery.min.js )
+ Rails.application.config.assets.precompile += %w( jquery.magnific-popup.min.js )
+ Rails.application.config.assets.precompile += %w( magnific-popup-options.js )
+ Rails.application.config.assets.precompile += %w( magnific-popup-options.js )
+ Rails.application.config.assets.precompile += %w( simplyCountdown.js )
+ Rails.application.config.assets.precompile += %w( tether.min.js )
+ Rails.application.config.assets.precompile += %w( modernizr-2.6.2.min.js )
+ Rails.application.config.assets.precompile += %w( jquery.waypoints.min.js )
+
+
+
+
